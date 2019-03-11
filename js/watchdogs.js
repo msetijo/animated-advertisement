8d5c07b02b98a3818f0d1cf34490082d31e80c37
Mela S <msetijo@gmail.com>
2019-03-10 20:28:06 -0400

    rollover animated updated

--- js/watchdogs.js
+++ js/watchdogs.js
@@ -1,20 +1,29 @@
 window.onload = function() {
 
     let ad = document.getElementById('ad');
+    let images = document.getElementsByClassName("images");
+
     let countdown = document.getElementById("countdown");
+
     let one = document.getElementById("one");
     let two = document.getElementById("two");
     let three = document.getElementById("three");
 
-    // play normal
+    // play animated
     ad.addEventListener('mouseover', () => {
         countdown.classList.add('animated');
         one.classList.add("one-animated");
         two.classList.add("two-animated");
         three.classList.add("three-animated");
+
+        //making it translucent
+        ad.classList.add("darken");
+        for (var i = 0; i < images.length; i++) {
+            images[i].style.animationPlayState = "paused";
+        }
     })
 
-    // play in reverse
+    // remove animation
     ad.addEventListener('mouseout', () => {
     
         countdown.classList.remove('animated');
@@ -22,9 +31,17 @@ window.onload = function() {
         two.classList.remove("two-animated");
         three.classList.remove("three-animated");
 
+        ad.classList.remove("darken");
+        for (var i = 0; i < images.length; i++) {
+            images[i].style.animationPlayState = "running";
+        }
+
         setTimeout(() => {
             countdown.classList.add('inactive');
             one.classList.add("inactive");
+
+            //removing translucent
+            ad.classList.add("undarken");
         }, 5)
 
     })
